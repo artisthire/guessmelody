bfc28c892e4e0d8da2e8afe526c0916df8623e00
Andrey <artist_hire@ukr.net>
2020-03-11 16:22:36 +0200

    Изменил файлы для отображения стартового окна игры на основе его презентера и представления

--- js/main.js
+++ js/main.js
@@ -1,35 +1,21 @@
-/**
- * Модуль главного файла
- * из которого инциализируются другие модули и запускается игра
- */
-import {startGame} from './controller.js';
 import {loadGameData} from './network/server-communication.js';
 import preloadResource from './network/resouce-preloader.js';
-import showModalError from './templates/modal-error.js';
-import {showLoadAnimation, removeLoadAnimation} from './templates/load-animation.js';
 
-showLoadAnimation();
+import Application from './application.js';
+import ModalLoadAnimationView from './views/modal-load-animatin-view.js';
+import ModalErrorView from './views/modal-error-view.js';
+import {showScreen} from './utilities.js';
+
+const loadAnimationElement = new ModalLoadAnimationView().element;
+
+showScreen(loadAnimationElement, false);
 
 // предзагрузка ресуров для игры:
 loadGameData()
 .then((gameData) => preloadResource(gameData))
-.then(() => onSuccessLoad()).catch(() => onErrorLoad());
-
-/**
- * Функция вызывается, когда были ошибки предзагрузки ресурсов для игры
- */
-function onErrorLoad() {
-  removeLoadAnimation();
-  showModalError();
-}
-
-/**
- * Функция вызывается, когда ресурсы для игры успешно загружены
- */
-function onSuccessLoad() {
-  removeLoadAnimation();
-  // показываем окно приветствия
-  startGame();
-}
-
-
+// в любом случае убираем анимацию процесса загрузки
+.finally(() => loadAnimationElement.remove())
+// при успешной загрузке стартуем игру
+.then(() => Application.showStart())
+// в случае ошибок, игру не стартуем, а показываем экран ошибки
+.catch(() => showScreen(new ModalErrorView().element, false));
