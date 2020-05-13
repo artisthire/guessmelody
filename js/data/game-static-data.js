06261349952416819d06feeb7fd1b366a7fb5941
Andrey <artist_hire@ukr.net>
2020-05-13 15:17:31 +0300

    Исправил адреса аудиофайлов в замоканых данных для загрузки без редиректа

--- js/data/game-static-data.js
+++ js/data/game-static-data.js
@@ -10,42 +10,42 @@ export default [
     artist: `Kevin MacLeod`,
     name: `Long Stroll`,
     image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Kevin_MacLeod/Oddities/Kevin_MacLeod_-_Sinfonia_3.mp3`,
     genre: `Jazz`
   },
   {
     artist: `Jingle Punks`,
     name: `In the Land of Rhinoplasty`,
     image: `https://i.vimeocdn.com/portrait/992615_300x300`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Kevin_MacLeod/Oddities/Kevin_MacLeod_-_Sing_Along_with_Jim.mp3`,
     genre: `Rock`
   },
   {
     artist: `Audionautix`,
     name: `Travel Light`,
     image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Jason_Shaw/Audionautix_Acoustic/Jason_Shaw_-_TRAVEL_LIGHT.mp3`,
     genre: `Country`
   },
   {
     artist: `Riot`,
     name: `Level Plane`,
     image: `https://yt3.ggpht.com/a/AGF-l7-BSwMnN5-kvIOxYVDXV1TRBG-G8LTRpjsHPQ=s48-c-k-c0xffffffff-no-rj-mo`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Steve_Combs/Riot/Steve_Combs_-_11_-_More_Or_Less_OK.mp3`,
     genre: `R&B`
   },
   {
     artist: `Jingle Punks`,
     name: `Lucky Day`,
     image: `https://i.vimeocdn.com/portrait/992615_300x300`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Jason_Shaw/Audionautix_Acoustic/Jason_Shaw_-_SERENITY.mp3`,
     genre: `Pop`
   },
   {
     artist: `Quincas Moreira`,
     name: `Firefly`,
     image: `https://assets.mubi.com/images/cast_member/535878/image-w240.jpg?1523578011`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+    src: `https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Steve_Combs/Riot/Steve_Combs_-_04_-_Its_Kicking_In.mp3`,
     genre: `Electronic`
   }
 ];
